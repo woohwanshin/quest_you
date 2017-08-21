5523ae2153f963416877bf291dee71fadb3e9f69
Woohwan Shin <woohwan5810@likelion.org>
2017-08-21 10:16:00 +0000

    For auto deploy script

--- db/migrate/20170817120838_add_friend_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFriendToUsers < ActiveRecord::Migration
-  def change
-    add_foreign_key :friends, :users, column: :sender_id, primary_key: :id
-    add_foreign_key :friends, :users, column: :receiver_id, primary_key: :id
-  end
-end
